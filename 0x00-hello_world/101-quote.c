13c646f498927ae011950268ae9485374e417dde
Mohammed Abdelrahim <mopilani1@gmail.com>
2023-08-10 10:52:35 -0700

    Hot fix 96

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,14 +9,11 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	long index = 0;
-	while (index < 59) 
+
+	while (index < 59)
 	{
 		putchar(str[index]);
 		index++;
 	}
-	/*for (int i = 0; i < sizeof(str); i++) {
-	*	putchar(str[i]);
-	*
-	* putchar('and that piece of art is useful - Dora Korpar, 2015-10-19');*/
-    return (1);
+	return (1);
 }
