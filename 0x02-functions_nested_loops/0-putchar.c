cddf5af926bbfdc7276d02b9a32ac92c7332e7d9
Mohammed Abdelrahim <mopilani1@gmail.com>
2023-08-14 21:05:08 -0700

    Add 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* This program will assign a random number to the variable */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
+
