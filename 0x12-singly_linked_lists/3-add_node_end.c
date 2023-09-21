cea3678fa50231e906b366abdaabe47a8b6f8293
Mohammed Abderrahim <mopilani1@gmail.com>
2023-09-21 14:28:49 -0700

    Add 3-main.c and solu

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_node_end - function that add node at the end.
+ * @head: an input from type list_t.
+ * @str: pointer to char.
+ * Return: the number of nodes in size_t.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ptr, *current;
+
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->str = strdup(str);
+	ptr->len = strlen(str);
+	ptr->next = NULL;
+	if (*head == NULL)
+	{
+		*head = ptr;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = ptr;
+	}
+	return (*head);
+}
